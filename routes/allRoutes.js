7c902556cbaaae7917cf758bae38514b82d57fde
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-07 18:22:44 -0700

    Installed Prisma with a simple User model

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { prisma } from "../lib/prisma.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -7,6 +8,27 @@ router.get("/", (req, res) => {
 
 router.get("/test", (req, res) => {
     res.json({ message: "Welcome to the Test path!" })
+})
+
+router.get("/users/:id", async (req, res) => {
+    try {
+        const userId = req.params.id;
+        // Remember, req.params.id is a STRING. Convert to a number
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(userId)
+            }
+        })
+        
+        if (!user) {
+            console.log(`User with ID ${ id } not found.`);
+            return null;
+        }
+
+        return res.json(user);
+    } catch (error) {
+        return res.status(400).json({ error: "Fatal error occurred when trying to retrieve this user from the database: " + error });
+    }
 })
 
 export default router;
